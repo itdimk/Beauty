f3873d3672288bcadef5f39c6f988ff34ec6526e
itdimk <dimk.97@ya.ru>
2023-02-21 11:55:32 +0300

    Added adaptivity

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
 ﻿import "./scss/style.scss"
+
+let isVisible = false;
+
+window.toggleNavbar = function () {
+    isVisible = !isVisible;
+    const nav = document.querySelector('.header__nav-items');
+    nav.style.display = isVisible ? "flex" : "none";
+}
